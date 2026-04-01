a8b914a3c8a49c6719f98107ccb700cfcf0c7576
vinglivskt <vinglivskt@gmail.com>
2026-04-01 11:07:28 +0300

    Добавить переменную окружения UPLOAD_DIR для тестов

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 
 os.environ.setdefault("PYTEST_RUNNING", "1")
+os.environ.setdefault("UPLOAD_DIR", "./data/uploads")
 os.environ.pop("LEGACY_MEDIA_IMPORT_DIR", None)
 os.environ.pop("FORCE_LEGACY_MEDIA_REIMPORT", None)
